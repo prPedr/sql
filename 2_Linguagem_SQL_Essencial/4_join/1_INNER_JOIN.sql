b59cd9c2ef1abfb4e57aae8af8523c3e748d54e0
prPedr <prpedro025@gmail.com>
2026-06-03 20:08:58 -0300

    Realizando consulta INNER JOIN

---
+++ 2_Linguagem_SQL_Essencial/4_join/1_INNER_JOIN.sql
@@ -0,0 +1,13 @@
+USE fullcycle;
+
+SELECT 
+    colaboradores.id,
+    colaboradores.nome_colaborador,
+    colaboradores.email_colaborador,
+    produtos.id AS produto_id, # Renomeando ID para nao causar conflito por conta do ID de colaboradores
+	produtos.marca_produto,
+    produtos.status_produto
+FROM
+    colaboradores
+INNER JOIN
+    produtos ON colaboradores.id = produtos.id_colaborador
